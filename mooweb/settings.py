8911d06c1e03df3eb20c4b5051ddbb4e47cb64ae
Alexis <arpa1990@hotmail.com>
2022-06-08 04:00:11 -0500

    configuraciones addicionales

--- mooweb/settings.py
+++ mooweb/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-w)56$xvs(6njr)$p(w0-@z&km*0qm^9@g#9q61=!o#$m-!_ob4'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
